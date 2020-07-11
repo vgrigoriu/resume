620489bbee3c874e7f6b6c5df230b661d8588483
Victor Grigoriu <grigoriu@adobe.com>
2020-07-11 16:25:03 +0300

    Update build.ps1

--- src/build.ps1
+++ src/build.ps1
@@ -1,3 +1,3 @@
-pandoc ..\README.md -o ..\index.html -s -A after.html -c src/style.css -M pagetitle="Victor Grigoriu $([char]0x2013) R$([char]0x00E9)sum$([char]0x00E9)"
-pandoc ..\README.md -o ..\resume_victor_grigoriu.docx
-pandoc ..\README.md -o ..\resume_victor_grigoriu.pdf
+pandoc ../README.md -o ../index.html -s -A after.html -c src/style.css -M pagetitle="Victor Grigoriu $([char]0x2013) R$([char]0x00E9)sum$([char]0x00E9)"
+pandoc ../README.md -o ../resume_victor_grigoriu.docx
+pandoc ../README.md -o ../resume_victor_grigoriu.pdf
